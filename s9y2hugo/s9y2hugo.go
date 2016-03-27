99454d0c7b91aa8292423fa0bbf532dfdf9e1bf6
Rodger Donaldson <rodger@donaldson.gen.nz>
2016-03-27 15:56:24 +1300

    Updated the go code and SQL to better reflect the available source data and outputs:
     * permalinks as an array.
    
     * Eliminate various fields that don't exist in the source even though they're in hugo.
    
     * Pull from postgresql as native array_agg output, instead of half-arsed JSON.

--- s9y2hugo/s9y2hugo.go
+++ s9y2hugo/s9y2hugo.go
@@ -14,17 +14,14 @@ aliases.
 The input spec is straightforward and derived from the serendipity-extract:
     Column    Source Description     				Output Field
     ------    ------------------	      	  ------------
-    [0]       TUnix epoch										Date in RFC3339 format
+    [0]       Unix epoch										Date in RFC3339 format
     [1]       Title			      							Title
-    [2]       Description
-    [3]       Comma-delimeted array of tags  Tags in the format ["tag1","tag2"]
-    [4]       Comma-delimeted array of categories
-					    															Categories in the format ["cat1","cat2"]
-    [5]       Permalink 		      					Aliases in the format ["alias1"]
-    [6]       The project URL		      			Dropped - this properly belongs in config.toml
-		[7]				isdraft												Whether the entry is draft or not.
-    [8]       Body						      				Body text.
-		[9]				extended											Any extended text.
+    [2]       Array of tags  								Tags in JSON format
+    [4]       Array of categories						Categories in JSON format
+    [5]       Array of permalinks 		      					Aliases in JSON format
+		[6]				isdraft												Whether the entry is draft or not.
+    [7]       Body						      				Body text.
+		[8]				extended											Any extended text.
 
 */
 
@@ -42,12 +39,13 @@ import (
 )
 
 type Post struct {
-     Date 	string
-     Title	string
-     Tags	[]string
+     Date 			string
+     Title			string
+     Tags	[]		string
      Categories	[]string
      Permalink	[]string
-     Body	string
+     Body				string
+		 Extended		string
 }
 
 const templ = `
@@ -59,7 +57,7 @@ Tags	= [{{range $index, $elmt := .Tags}}{{if $index}},"{{$elmt}}"{{else}}"{{$elm
 Aliases = [{{range $index, $elmt := .Permalink}}{{if $index}},"{{$elmt}}"{{else}}"{{$elmt}}"{{end}}{{end}}]
 +++
 {{.Body}}
-
+{{.Extended}}
 `
 
 func main() {
@@ -72,26 +70,29 @@ func main() {
 	// r := csv.NewReader(strings.NewReader(in))
 
 	posts := []Post{
-		{		     Title:	"French Toast",
-				     Date:	"1132985220",
-				     Tags:	[]string{"food", "wellington"},
-				     Categories: []string{"Food"},
-				     Permalink: []string{"archives/775-French-Toast"},
-				     Body: 	"<p>I'm just not that fond of french toast as it appears in most Wellington cafés. Mostly it's done with thick bread and incredibly sweet, usually with fruit or maple syrup.</p><p>I grew up with french toast being a savoury treat: egg, milk, pepper, and salt whisked together, with plain toast slice bread dunked into it and then pan-fried in butter until golden brown. Dee-licious.</p>",
+		{
+			Title:	"French Toast",
+			Date:	"1132985220",
+			Tags:	[]string{"food", "wellington"},
+			Categories: []string{"Food"},
+			Permalink: []string{"archives/775-French-Toast"},
+			Body: 	"<p>I'm just not that fond of french toast as it appears in most Wellington cafés. Mostly it's done with thick bread and incredibly sweet, usually with fruit or maple syrup.</p><p>I grew up with french toast being a savoury treat: egg, milk, pepper, and salt whisked together, with plain toast slice bread dunked into it and then pan-fried in butter until golden brown. Dee-licious.</p>",
+			Extended: "",
 		},
 		{
-				Title:	"Bubba Ho-Tep",
-				Date:	"1132985220",
-				     Tags:	[]string{"food", "wellington"},
-				     Categories: []string{"Movies"},
-				     Permalink: []string{"archives/776-Bubba-Ho-Tep"},
-				     Body: 	`<p>The premise of <a href="http://www.imdb.com/title/tt0281686/">Bubba Ho-Tep</a> is bizarre and amusing: Elvis lives.  In a rest home.  With a black guy who thinks he&#8217;s JFK.  A mummy is stalking the corridors and, well, &#8220;ask not what your rest home can do for you, but what you can do for your rest home.&#8221;</p>
+			Title:	"Bubba Ho-Tep",
+			Date:	"1132985220",
+			Tags:	[]string{"food", "wellington"},
+		 	Categories: []string{"Movies"},
+			Permalink: []string{"archives/776-Bubba-Ho-Tep"},
+			Body: 	`<p>The premise of <a href="http://www.imdb.com/title/tt0281686/">Bubba Ho-Tep</a> is bizarre and amusing: Elvis lives.  In a rest home.  With a black guy who thinks he&#8217;s JFK.  A mummy is stalking the corridors and, well, &#8220;ask not what your rest home can do for you, but what you can do for your rest home.&#8221;</p>
 
 					 	<p>The thing is, it&#8217;s creepy, but not like you&#8217;d expect&#8230;</p>
 
 					 	<p>As a horror movie, it&#8217;s pretty much what you&#8217;d expect from anything with <a href="http://www.imdb.com/name/nm0132257/">Bruce Campbell</a>: a good, solid, slightly scary, tongue in cheek horror.  The thing is it&#8217;s actually creepier at the start than once the movie gets into full swing&#151;and mummies have nothing to do with it.  Rather, it&#8217;s Campbell&#8217;s confused old man in a shitty rest home that sent chills up and down my spine.</p>
 
 					 	<p>It&#8217;s enough to make you hope you don&#8217;t ever grow old.</p>`,
+			Extended: "",
 		},
 	}
 
--- sql/serendipity-extract.sql
+++ sql/serendipity-extract.sql
@@ -1,24 +1,26 @@
 copy (
-select	e.timestamp, 
-	e.title, 
-	'' as description, 
-	'["' || (
-		select 	array_to_string(array_agg(tag), '","')
-		from 	serendipity_entrytags where entryid = e.id
-	) || '"]' as tags, 
-	'["' || (
-		select	array_to_string(array_agg(category_name), '","') 
-		from  	serendipity_category c, serendipity_entrycat ec 
-		where  	c.categoryid = ec.categoryid and 
-			ec.entryid = e.id
-	) || '"]' as topics, 
-	-- '' as slug,	-- slug is a relative path to the web root
-	p.permalink as url,	-- URL gives a hardcoded absolute path
-	'http://diaspora.gen.nz/~rodgerd/' as project_url,
-	e.body as body 
-from	serendipity_entries e, serendipity_permalinks p
-where 	e.id = p.entry_id and
-	e.id = 1334 
-order by e.timestamp 
+select	e.timestamp,
+	e.title,
+	(
+		select 	array_agg(tag)
+		from		serendipity_entrytags where entryid = e.id
+	) as tags,
+	(
+		select	array_agg(category_name)
+		from  	serendipity_category c, serendipity_entrycat ec
+		where  	c.categoryid = ec.categoryid and
+						ec.entryid = e.id
+	) as categories,
+	(
+		select	array_agg(permalink)
+		from		serendipity_permalinks p
+		where		p.entry_id = e.id
+	) as url,
+	e.isdraft as isdraft,
+	e.body as body,
+	e.extended as extended
+from	serendipity_entries e
+where 	e.id = 1334
+order by e.timestamp
 limit 1
 ) to stdout with csv;
